05bfec613bfd9d6e42bb061b9c2b5ab7d23bce92
tpugh-ia <tpugh@iastate.edu>
2024-06-03 17:12:57 -0700

    Update and rename W1P1.py to WIP1.py

--- W1P1.py
+++
@@ -1,2 +0,0 @@
-# Code for new file and new pull request
-print("This is file W1P1 to initiate a new pull request")
---
+++ WIP1.py
@@ -0,0 +1,2 @@
+# Code for new file and new pull request
+print("This is file WIP1 to initiate a new pull request")
